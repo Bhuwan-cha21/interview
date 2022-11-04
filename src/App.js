86eb8334ad9a4d9598f121c328d4a39c5c78d64d
Bhuwan-cha21 <53478182+Bhuwan-cha21@users.noreply.github.com>
2022-11-04 21:47:58 +0545

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {  useRef, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const todoHead = useRef(); 
+  const todoDetails = useRef(); 
+  
+
+  function delTodo(index){   //deletefunction
+    var newList = todos.filter((item,key) =>{
+      return key !== index
+    })
+    setTodos(newList)
+  }
+  function addTodo(e) {
+    e.preventDefault();
+    if (todoHead.current.value.trim() != ''){
+          setTodos([...todos,
+            {
+              todoHead:todoHead.current.value,
+              todoDetails: todoDetails.current.value,
+              isCompleted: false
+            
+          }])
+          todoHead.current.value = ''
+        todoDetails.current.value = ''
+    }
+    
+  }
+  function completeTodo(index){
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+  function uncompleteTodo(index){
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = false;
+    setTodos(newTodos);
+  }
+  return(
+    <div className='Container'>
+      
+        <div className='Todo'>
+        <div className='form-container'>
+         <h1 className='form-title'>Add Todo</h1>
+            <form className='form'>
+              <input type="text"  className='input-field' placeholder='Todo HeadLine' ref={todoHead} />
+              <input type="text"  className='input-field' placeholder='Todo Details' ref={todoDetails} />
+              <button className='btn' onClick={addTodo}>Add</button>
+            </form>
+        </div>
+            {todos.map((details,index) =>{
+              return(
+              <div className='Item'>
+                <span className='todohead' style={{ textDecoration: details.isCompleted ? "line-through" : "" }}>{details.todoHead}</span>
+                <p className='tododetails'>
+                  
+                  {details.todoDetails}
+                </p>
+                
+                <button className='btn' onClick={()=>{
+                    delTodo(index)
+                }}>Delete</button>
+                {details.isCompleted ? <button className='btn' onClick={()=>{
+                    uncompleteTodo(index)
+                }}>Complete</button> : <button className='btn' onClick={()=>{
+                    completeTodo(index)
+                }}>Uncomplete</button>}
+                
+                 
+              </div>
+              )
+            })
+          }
+        
+
+        
+        </div>
+  </div>
+  )
 }
-
-export default App;
+      
